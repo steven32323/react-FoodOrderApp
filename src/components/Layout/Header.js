bf26c249168b2972f903aca4167cac6622a4eb8b
steven32323 <107620513+steven32323@users.noreply.github.com>
2023-03-24 13:21:08 +0000

    added navbar, cart button, and basic meal info/styling

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import mealsImg from "../../Assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="A table full of delicious food!" />
+      </div>
+    </>
+  );
+};
+export default Header;
